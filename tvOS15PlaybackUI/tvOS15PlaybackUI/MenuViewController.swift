55edbe903039cefb619c456e5fc3a9d0eca6617c
Morgan <nihao0705@gmail.com>
2022-02-28 21:59:36 +0800

    establish menu page

---
+++ tvOS15PlaybackUI/tvOS15PlaybackUI/MenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tvOS15PlaybackUI
+//
+//  Created by Zheng-Yuan Yu on 2022/2/28.
+//
+
+import UIKit
+import TVUIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet var posterView: TVPosterView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+    }
+}
+
